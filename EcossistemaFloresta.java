29d4b4049810283f052cdcaa1f5bc3e0bd02dc68
RickRch <147344763+RickRch@users.noreply.github.com>
2023-12-02 11:57:37 -0300

    Update EcossistemaFloresta.java

--- EcossistemaFloresta.java
+++ EcossistemaFloresta.java
@@ -6,64 +6,44 @@ public class EcossistemaFloresta {
     public static void main(String[] args) {
         List<SerVivo> seresVivos = new ArrayList<>();
         Random random = new Random();
+
         // Criar uma população de animais e árvores
-        for (int i = 1; i <= 3; i++) {
-            Onça onça = new Onça("Onça " + i, random.nextInt(10) + 1);
-            seresVivos.add(onça);
-        }
-        for (int i = 1; i <= 3; i++) {
-            Tamandua tamandua = new Tamandua("Tamandua " + i, random.nextInt(5) + 1);
-            seresVivos.add(tamandua);
-        }
-        for (int i = 1; i <= 10; i++) {
-            Arvore arvore = new Arvore("Árvore " + i, random.nextInt(30) + 1);
-            seresVivos.add(arvore);
-        }
+        criarPopulacao(seresVivos, random, 3, Onca::new);
+        criarPopulacao(seresVivos, random, 3, Tamandua::new);
+        criarPopulacao(seresVivos, random, 10, Arvore::new);
 
         // Loop do jogo
         for (int tempo = 1; tempo <= 5; tempo++) {
             System.out.println("Tempo " + tempo + ":");
 
             // Gerar aleatoriamente um fenômeno natural
-            FenomenoNatural fenomeno = null;
-            double chanceFenomeno = random.nextDouble();
-            if (chanceFenomeno < 0.2) {
-                fenomeno = new Chuva();
-            } else if (chanceFenomeno < 0.4) {
-                fenomeno = new Tempestade();
-            }
+            FenomenoNatural fenomeno = gerarFenomeno(random);
 
             if (fenomeno != null) {
                 System.out.println(fenomeno.getNome() + " começou.");
             }
 
             for (SerVivo ser : seresVivos) {
-                if (ser instanceof Animal) {
-                    Animal animal = (Animal) ser;
-                    animal.mover();
-                    animal.comer();
-                    animal.crescer();
-
-                    if (animal instanceof Tamandua) {
-                        Tamandua tamandua = (Tamandua) animal;
-                        if (fenomeno instanceof Chuva) {
-                            tamandua.reproduzir();
-                        }
-                    } else if (animal instanceof Onça) {
-                        Onça onça = (Onça) animal;
-                          onça.caça();
-                    }
-                } else if (ser instanceof Arvore) {
-                    Arvore arvore = (Arvore) ser;
-                    arvore.crescer();
-                    if (fenomeno instanceof Chuva) {
-                        arvore.reproduzir();
-                    }
-                }
+                ser.realizarAcoes(fenomeno);
             }
 
             System.out.println("Fim do Tempo " + tempo + "\n");
         }
     }
 
+    private static void criarPopulacao(List<SerVivo> seresVivos, Random random, int quantidade, CriadorSerVivo criador) {
+        for (int i = 1; i <= quantidade; i++) {
+            seresVivos.add(criador.criar("SerVivo " + criador.getTipo() + " " + i, random.nextInt(criador.getMaxIdade()) + 1));
+        }
     }
+
+    private static FenomenoNatural gerarFenomeno(Random random) {
+        double chanceFenomeno = random.nextDouble();
+        if (chanceFenomeno < 0.9) {
+            return new Chuva();
+        } else if (chanceFenomeno < 0.6) {
+            return new Tempestade();
+        }
+        return null;
+    }
+}
